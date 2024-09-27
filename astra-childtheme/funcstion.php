273cf69bcc5fa2966b4ad5efde3cb98769147b75
Troy Choknakawaro <tanaporn.choknakawaro@my.jcu.edu.au>
2024-09-27 19:00:54 +0700

    Removed child theme folder

--- astra-childtheme/funcstion.php
+++
@@ -1,6 +0,0 @@
-<?php
-function astra_child_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'), wp_get_theme()->get('Version'));
-}
-add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles');
